02b503dd99fd15735887caa38ac577ec4d186a40
1079058 <1079058@cegepoutaouais.qc.ca>
2023-01-16 11:39:58 -0500

    16 janvier - 2

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function ajouter(){
-    var item = document.getElementById("desc").value;
+    // Entree de valeur dans un element
+    let item = document.getElementById("desc").value;          // id="desc"
     let nouveauP = document.createElement("p");                // Creer une balise <p>
-    nouveauP.innerText = item;                                         // Ecrire dans le paragraphe
-    document.getElementById("maListe").append(nouveauP);      // Ajouter le paragraphe a la liste
+    nouveauP.innerText = item;                                         // Ecrire dans le <p>
+    document.getElementById("maListe").append(nouveauP);      // Ajouter le paragraphe a maListe
+    let prix = document.getElementById("valeur").value;
+    let nouveauS = document.createElement("span");            // Creer une balise <span>
+    nouveauS.innerText = prix + "$";                                  // Ecrire dans le <span>
+    nouveauP.append(nouveauS);                                        // Concatener a nouveauP
+
+    // Mettre a jour le total
+    let ancienPrix = document.getElementById("total").innerText;
+    let nouveauPrix = +ancienPrix + +prix;                            // +variable : convertir en numerique
+    document.getElementById("total").innerText = nouveauPrix;
+
 
     // alert("L'item est bien ajouté"); // Alerte Pop-up
 }
